653f36154d89d26375720ed69dea9c4f205fdec5
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-03 03:06:08 +0530

    Update Employee_Bonus.sql

--- 577. Employee Bonus/Employee_Bonus.sql
+++ 577. Employee Bonus/Employee_Bonus.sql
@@ -1,1 +1,5 @@
-select Employee.name, Bonus.bonus from Employee left join Bonus on Employee.empID = Bonus.empID where bonus < 1000 or bonus is NULL;
+select Employee.name, Bonus.bonus
+from Employee
+left join Bonus
+on Employee.empID = Bonus.empID
+where bonus < 1000 or bonus is NULL;
